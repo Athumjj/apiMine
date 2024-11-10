cdd7d86f35f86fe53619d125c8ab84e1b08d22cb
Athumjj <melloteam22@gmail.com>
2024-11-10 12:26:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ app.use(express.json());
 
 app.post("/", (req, res) => {
     const { value } = req.body;
-    app.set("nickVar", value);
-    res.json({ value: "sucess" });
+    if (value) {
+        app.set("nickVar", value);
+        res.json({ valuea: "sucess" });
+    }
 });
 
 app.get("/obter", (req, res) => {
     const nicke = app.get("nickVar");
     if (nicke) {
-        res.end(nicke);
+        res.json({ value: nicke });
     }else {
         res.status(400).json({ erro: "nick não obtido." });
     }
