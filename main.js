4db58986ad035e6a1bcd92cf370280d45af9e7c9
Athumjj <melloteam22@gmail.com>
2024-11-10 11:21:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const nicks = [];
+
+app.post("/", (req, res) => {
+    const { value } = req.body;
+    if (!nicks.includes(value)) {
+        nicks.push(value);
+        app.set("nickVar", nicks);
+    }
+    res.end("sucess");
+});
+
+app.get("/obter", (req, res) => {
+    if (app.get("nickVar")) {
+        res.end(app.get("nickVar"));
+    }else {
+        res.status(400).send("Erro: nick não obtido.");
+    }
+});
+
+app.listen(process.env.PORT || 3000);
